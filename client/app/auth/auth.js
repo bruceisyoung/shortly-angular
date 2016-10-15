82f4574bb605fced96dd82dae6bd6e3c123efbf8
bruceisyoung <isyoung.bruce@gmail.com>
2016-10-14 20:00:38 -0700

    checking valid username/password and url

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -5,26 +5,45 @@ angular.module('shortly.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location, Auth) {
   $scope.user = {};
+  $scope.isValid = '';
 
   $scope.signin = function () {
-    Auth.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if ($scope.isValidUsername()) {
+      Auth.signin($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          $scope.isValid = 'Invalid Username or Password';
+          console.error(error);
+        });
+    } else {
+      $scope.isValid = 'Invalid Username or Password';
+    }
+
   };
 
   $scope.signup = function () {
-    Auth.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if ($scope.isValidUsername() && $scope.isValidPassword()) {
+      Auth.signup($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    } else {
+      $scope.isValid = 'Invalid Username or Password';
+    }
+  };
+
+  $scope.isValidUsername = function() {
+    return !$scope.user.username.match(/\W/);
+  };
+
+  $scope.isValidPassword = function() {
+    return !$scope.user.password.match(/\W/);
   };
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,26 @@
 //ToDo: figure out what $location means.
 angular.module('shortly.shorten', [])
 
+
+
+
 .controller('ShortenController', function ($scope, $location, Links) {
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+  var isValidUrl = function(url) {
+    return url.match(rValidUrl);
+  }; 
   $scope.link = {};
+
   $scope.addLink = function() {
-    $scope.link = $scope.newLink;
-    Links.addOne({ url: $scope.link });
+  $scope.isValid = '';
+    if (isValidUrl($scope.newLink)) {
+      $scope.link = $scope.newLink;
+      Links.addOne({ url: $scope.link });
+    } else {
+      $scope.isValid = 'invalid URL';
+    }
   };
 });
+
+
+
